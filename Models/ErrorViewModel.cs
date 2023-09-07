f4c667d047d55c58e14e6f2e089b51a06dd3e3c2
Pedro Hugo Heinen <pedro.hugo.heinen@live.com>
2023-09-07 15:25:01 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace entityframework_postgresql.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
