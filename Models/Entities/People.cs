3762163d2bf9493e86d5dd1fb17f60ec900fd32e
Pedro Hugo Heinen <pedro.hugo.heinen@live.com>
2023-09-07 16:24:58 -0300

    Added to entity, context and view classes

---
+++ Models/Entities/People.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace entityframework_postgresql.Models.Entities
+{
+    [Table("Peoples")]
+    public class People
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string RecoveryEmail { get; set; }
+
+        public string? Contact { get; set; }
+
+        [Required]
+        public DateTimeOffset BirthDate { get; set; }
+
+        [Required]
+        public DateTimeOffset CreateDateTime { get; set; }
+
+        public DateTimeOffset? UpdateDateTime { get; set;}
+    }
+}
